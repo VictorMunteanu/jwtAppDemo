4203abdd06ce5346634dc5b4f772c7a07d4ba7af
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-11 12:41:41 +0300

    updated Role class

--- src/main/java/com/example/jwtAppDemo/model/Role.java
+++ src/main/java/com/example/jwtAppDemo/model/Role.java
@@ -3,9 +3,8 @@ package com.example.jwtAppDemo.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "roles")
@@ -14,4 +13,7 @@ public class Role extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private List<User> users;
 }
