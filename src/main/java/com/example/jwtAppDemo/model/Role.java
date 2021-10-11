d9514a583fb0b287139504d9634a8dfaaa819c3a
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-11 12:18:39 +0300

    created Role class

---
+++ src/main/java/com/example/jwtAppDemo/model/Role.java
@@ -0,0 +1,17 @@
+package com.example.jwtAppDemo.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+}
